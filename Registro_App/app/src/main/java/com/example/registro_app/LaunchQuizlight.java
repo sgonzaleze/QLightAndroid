cf42bf7e850142d0a8395055cd8157426099898c
sgonzalez <sgonzaleze2021@cepnet.net>
2021-11-08 10:13:24 +0100

    Splash Screen hecho

---
+++ Registro_App/app/src/main/java/com/example/registro_app/LaunchQuizlight.java
@@ -0,0 +1,44 @@
+package com.example.registro_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaunchQuizlight extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+       setContentView(R.layout.launch_quizlight);
+
+
+       //Animaciones
+
+        Animation animacion1 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_abajo);
+
+        TextView launchText = findViewById(R.id.launch_title);
+        ImageView launchImage = findViewById(R.id.imageLaunch);
+
+
+        launchText.setAnimation(animacion2);
+        launchImage.setAnimation(animacion1);
+
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+             Intent intent = new Intent(LaunchQuizlight.this,LoginActivity.class);
+             startActivity(intent);
+             finish();
+            }
+        },4000);
+    }
+}
