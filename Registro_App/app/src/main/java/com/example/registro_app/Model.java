a1f810691dc384a6bebb042b77748a316298f65b
sgonzalez <sgonzaleze2021@cepnet.net>
2021-11-09 13:34:42 +0100

    scrollbar

---
+++ Registro_App/app/src/main/java/com/example/registro_app/Model.java
@@ -0,0 +1,4 @@
+package com.example.registro_app;
+
+public class Model {
+}
---
+++ Registro_App/app/src/main/java/com/example/registro_app/Usuario.java
@@ -0,0 +1,9 @@
+package com.example.registro_app;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Usuario {
+    List Usuarios = Collections.synchronizedList(new ArrayList());
+}
